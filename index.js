d2f5928d78dca3bd10a70adc2ead986484e111c7
noydono <th.gabillard@laposte.net>
2022-04-09 12:22:25 +0200

    rename repo

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const handleCommand = require('./helpers/command');
 
 let queue = new Map()
 
+
 require('dotenv').config();
 
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES]})
